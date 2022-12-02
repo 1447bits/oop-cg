fbdc2ba2eb462ae40d124523cb5d8c662ca98203
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2022-12-02 12:15:12 +0530

    Create Templete_programs.cpp

---
+++ Templete_programs.cpp
@@ -0,0 +1,35 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+// single args templete example 
+
+template <class T>
+void disp (T a, T b){
+    cout << "a = " << a << " b = " << b << endl;
+} 
+
+template <class T>
+void sub (T a, T b) {
+    cout << "a - b = " << a-b << endl;
+}
+
+
+// multiple type templete example
+template <typename T1, typename T2>
+void disp (T1 a, T2 b) {
+    cout << "a = " << a << " b = " << b << endl;    
+}
+template <typename T1, typename T2>
+void add (T)
+
+int main() {
+    disp(40, 20);
+    sub<int>(40, 20);
+    
+// multiple type templete example 
+    
+    disp(10, "one");
+    
+    return 0;
+}
